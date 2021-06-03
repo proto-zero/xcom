85fc5c3e679348148beca6bf4ce4168aabd141a4
Sean Lenhart <theseanlenhart@gmail.com>
2021-06-03 12:25:31 -0400

    update todo

---
+++ xcom.rb
@@ -0,0 +1,20 @@
+require "colorize"
+
+
+#### TODO
+# create arena
+# place players
+## choose player tokens
+# move players
+# turn actions
+## move up to a set amount of spaces
+## shoot
+### shooting increases in accuracy as distance to the target decreases
+### damage increases with accuracy
+## special actions
+### aim
+### sprint
+# hit points
+# player defeated
+# AI movement
+# AI turns
